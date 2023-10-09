ec1b9f2e6e8b622581783d95bb4f95b3a0d5ee99
Kibelevich <Hand4c28@mbjcomp.org.il>
2023-10-09 23:23:30 +0300

    Welcome function

---
+++ dotNet5784_5523_9161/Stage0/Program5523.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome5523();
+            Welcome9161();
+            Console.ReadKey();
+        }
+
+        private static void Welcome5523()
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application");
+        }
+        static partial void Welcome9161();
+
+    }
+}
