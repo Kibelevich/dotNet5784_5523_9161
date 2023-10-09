7a8ea5dde15eb0eb0bfcc0fa3fc974c33ac9998f
devora8586 <devora8586@gmail.com>
2023-10-10 00:10:35 +0300

    write hello

---
+++ dotNet5784_5523_9161/Stage0/Program9161.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static partial void Welcome9161()
+        {
+
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
