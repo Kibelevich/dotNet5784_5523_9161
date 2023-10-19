da81283ff7504803a0c36db094623000d10bc4f4
Kibelevich <Hand4c28@mbjcomp.org.il>
2023-10-19 11:35:54 +0300

    chapter 7

--- dotNet5784_5523_9161/DalList/EngineerImplementation.cs
+++ dotNet5784_5523_9161/DalList/EngineerImplementation.cs
@@ -2,8 +2,32 @@
 namespace Dal;
 using DalApi;
 using DO;
+using System.Collections.Generic;
 
-public class EngineerImplementation
+public class EngineerImplementation : IEngineer
 {
+    public int Create(Engineer item)
+    {
+        throw new NotImplementedException();
+    }
 
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Engineer? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Engineer> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Engineer item)
+    {
+        throw new NotImplementedException();
+    }
 }
