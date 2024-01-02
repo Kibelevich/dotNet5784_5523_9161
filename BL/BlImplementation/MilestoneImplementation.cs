1b2b53ecbec1bcf7476ea9f785dea7b7b9ef8786
מימרן דבורה <devora8586@gmail.com>
2024-01-02 19:12:08 +0200

    without luz

--- BL/BlImplementation/MilestoneImplementation.cs
+++ BL/BlImplementation/MilestoneImplementation.cs
@@ -6,6 +6,7 @@ internal class MilestoneImplementation : IMilestone
 {
     private DalApi.IDal _dal = DalApi.Factory.Get;
     private IBl bl = Factory.Get();
+
     public BO.Milestone? Read(int id)
     {
         DO.Task? doTask = _dal.Task.Read(id);
@@ -85,114 +86,114 @@ internal class MilestoneImplementation : IMilestone
         return (100 * doneTasks) / tasksAmount;
     }
 
-    public void CreateSchedual(DateTime startProject, DateTime endProject)
-    {
-        CreateMilestones();
-        calcTimes(startProject,endProject);
+    //public void CreateSchedual(DateTime startProject, DateTime endProject)
+    //{
+    //    CreateMilestones();
+    //    calcTimes(startProject,endProject);
 
-    }
+    //}
 
-    void CreateMilestones()
-    {
-        int nextId = 1;
-        _dal.Task.ReadAll(task => _dal.Dependency.ReadAll(depend => depend.dependentTask == task.ID) == null)
-            .Where(task => task != null).Select(task => CreateStartMilestone(task!.ID));
-        IEnumerable <(int? key, IEnumerable<DO.Dependency> dependencies)> list =
-            (from dependency in _dal.Dependency.ReadAll()
-            group dependency by dependency.dependentTask into dependGroup
-            orderby dependGroup.Key
-            select (dependGroup.Key, dependGroup.Select(depend=>depend)));
-        IEnumerable<int> milestoneIDs = list
-            .Select(task => CreateMilestone(task.key, task.dependencies, nextId++));
-        _dal.Task.ReadAll(task => _dal.Dependency.ReadAll(depend => depend.dependsOnTask == task.ID) == null)
-            .Where(task => task != null).Select(task => CreateEndMilestone(task!.ID));
-    }
+    //void CreateMilestones()
+    //{
+    //    int nextId = 1;
+    //    _dal.Task.ReadAll(task => _dal.Dependency.ReadAll(depend => depend.dependentTask == task.ID) == null)
+    //        .Where(task => task != null).Select(task => CreateStartMilestone(task!.ID));
+    //    IEnumerable <(int? key, IEnumerable<DO.Dependency> dependencies)> list =
+    //        (from dependency in _dal.Dependency.ReadAll()
+    //        group dependency by dependency.dependentTask into dependGroup
+    //        orderby dependGroup.Key
+    //        select (dependGroup.Key, dependGroup.Select(depend=>depend)));
+    //    IEnumerable<int> milestoneIDs = list
+    //        .Select(task => CreateMilestone(task.key, task.dependencies, nextId++));
+    //    _dal.Task.ReadAll(task => _dal.Dependency.ReadAll(depend => depend.dependsOnTask == task.ID) == null)
+    //        .Where(task => task != null).Select(task => CreateEndMilestone(task!.ID));
+    //}
 
-    int CreateMilestone(int? key, IEnumerable<DO.Dependency> dependencies,int nextId)
-    {
-        int? id;
-        id = (from milestone in bl.Milestone.ReadAll()
-              where  milestone.dependencies.Any(depend1 => depend1!=null?dependencies.Any(depend2 => depend2.dependsOnTask == depend1.ID):false)
-              select milestone.ID).FirstOrDefault();
-        if (id == null)
-        {
-            id = _dal.Task.Create(new DO.Task(0, $"Milestone number: {nextId}", $"M{nextId}", true,
-                TimeSpan.Zero, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue,
-                DateTime.MinValue, "", "", null, null));
-        }
-            IEnumerable<int> dependIDs = from DO.Dependency dependTask in dependencies
-                                         select _dal.Dependency.Create(dependTask with { dependentTask = id });
-        var i = from DO.Dependency dependTask in dependencies
-                where dependTask.dependentTask == key
-                select DeleteDependency(dependTask.ID);
-        _dal.Dependency.Create(new DO.Dependency(0, key, id));
-        return (int)id;
-    }
+    //int CreateMilestone(int? key, IEnumerable<DO.Dependency> dependencies,int nextId)
+    //{
+    //    int? id;
+    //    id = (from milestone in bl.Milestone.ReadAll()
+    //          where  milestone.dependencies.Any(depend1 => depend1!=null?dependencies.Any(depend2 => depend2.dependsOnTask == depend1.ID):false)
+    //          select milestone.ID).FirstOrDefault();
+    //    if (id == null)
+    //    {
+    //        id = _dal.Task.Create(new DO.Task(0, $"Milestone number: {nextId}", $"M{nextId}", true,
+    //            TimeSpan.Zero, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue,
+    //            DateTime.MinValue, "", "", null, null));
+    //    }
+    //        IEnumerable<int> dependIDs = from DO.Dependency dependTask in dependencies
+    //                                     select _dal.Dependency.Create(dependTask with { dependentTask = id });
+    //    var i = from DO.Dependency dependTask in dependencies
+    //            where dependTask.dependentTask == key
+    //            select DeleteDependency(dependTask.ID);
+    //    _dal.Dependency.Create(new DO.Dependency(0, key, id));
+    //    return (int)id;
+    //}
 
-    int CreateStartMilestone(int taskID)
-    {
-        if (bl.Milestone.Read(milestone => milestone.alias == "START") == null)
-            _dal.Task.Create(new DO.Task(0, "Start milestone", "START", true,
-               TimeSpan.Zero, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue,
-               DateTime.MinValue, "", "", null, null));
-        _dal.Dependency.Create(new DO.Dependency(0, taskID, 0));
-        return 0;
-    }
+    //int CreateStartMilestone(int taskID)
+    //{
+    //    if (bl.Milestone.Read(milestone => milestone.alias == "START") == null)
+    //        _dal.Task.Create(new DO.Task(0, "Start milestone", "START", true,
+    //           TimeSpan.Zero, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue,
+    //           DateTime.MinValue, "", "", null, null));
+    //    _dal.Dependency.Create(new DO.Dependency(0, taskID, 0));
+    //    return 0;
+    //}
 
-    int CreateEndMilestone(int taskID)
-    {
-        int id;
-        BO.Milestone? endMilestone = bl.Milestone.Read(milestone => milestone.alias == "END");
-        if (endMilestone == null)
-            id = _dal.Task.Create(new DO.Task(0, "End milestone", "END", true,
-                TimeSpan.Zero, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue,
-                DateTime.MinValue, "", "", null, null));
-        else
-            id = endMilestone.ID;
-        _dal.Dependency.Create(new DO.Dependency(0,id , taskID));
-        return id; 
-    }
+    //int CreateEndMilestone(int taskID)
+    //{
+    //    int id;
+    //    BO.Milestone? endMilestone = bl.Milestone.Read(milestone => milestone.alias == "END");
+    //    if (endMilestone == null)
+    //        id = _dal.Task.Create(new DO.Task(0, "End milestone", "END", true,
+    //            TimeSpan.Zero, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue, DateTime.MinValue,
+    //            DateTime.MinValue, "", "", null, null));
+    //    else
+    //        id = endMilestone.ID;
+    //    _dal.Dependency.Create(new DO.Dependency(0,id , taskID));
+    //    return id; 
+    //}
 
-    int DeleteDependency( int id)
-    {
-        _dal.Dependency.Delete(id);
-        return id;
-    }
+    //int DeleteDependency( int id)
+    //{
+    //    _dal.Dependency.Delete(id);
+    //    return id;
+    //}
 
-    void calcTimes(DateTime startProject, DateTime endProject)
-    {
-       BO.Milestone firstMilestone = bl.Milestone.ReadAll().First();
-        bl.Milestone.Update(new BO.Milestone() { 
-            ID=firstMilestone.ID,
-            alias = firstMilestone.alias,
-            description = firstMilestone.description,
-            status = firstMilestone.status,
-            createdAt = firstMilestone.createdAt,
-            baselineStart = firstMilestone.baselineStart,
-            start=firstMilestone.start,
-            forecastEndDate=firstMilestone.forecastEndDate,
-            deadline= endProject,
-            complete=firstMilestone.complete,
-            completionPercentage=firstMilestone.completionPercentage,
-            remarks=firstMilestone.remarks,
-            dependencies=firstMilestone.dependencies
-        });
-        IEnumerable<BO.Milestone> milestonses = bl.Milestone.ReadAll().Reverse();
-        var i = from milestone in milestonses
-                where milestone.alias != "START"
-                let minTime= milestone.deadline - milestone.dependencies
-                    .Max(depend=>depend != null ?_dal.Task.Read(depend.ID)!.requiredEffortTime:TimeSpan.Zero)
-                select (milestone.dependencies.Select(depend =>
-                {
-                    DO.Task? task = depend == null ? null : _dal.Task.Read(depend.ID);
-                    if (task != null)
-                        _dal.Task.Update(task with { deadline = milestone.deadline });
-                    return task;
-                }));                  
-    }
+    //void calcTimes(DateTime startProject, DateTime endProject)
+    //{
+    //   BO.Milestone firstMilestone = bl.Milestone.ReadAll().First();
+    //    bl.Milestone.Update(new BO.Milestone() { 
+    //        ID=firstMilestone.ID,
+    //        alias = firstMilestone.alias,
+    //        description = firstMilestone.description,
+    //        status = firstMilestone.status,
+    //        createdAt = firstMilestone.createdAt,
+    //        baselineStart = firstMilestone.baselineStart,
+    //        start=firstMilestone.start,
+    //        forecastEndDate=firstMilestone.forecastEndDate,
+    //        deadline= endProject,
+    //        complete=firstMilestone.complete,
+    //        completionPercentage=firstMilestone.completionPercentage,
+    //        remarks=firstMilestone.remarks,
+    //        dependencies=firstMilestone.dependencies
+    //    });
+    //    IEnumerable<BO.Milestone> milestonses = bl.Milestone.ReadAll().Reverse();
+    //    var i = from milestone in milestonses
+    //            where milestone.alias != "START"
+    //            let minTime= milestone.deadline - milestone.dependencies
+    //                .Max(depend=>depend != null ?_dal.Task.Read(depend.ID)!.requiredEffortTime:TimeSpan.Zero)
+    //            select (milestone.dependencies.Select(depend =>
+    //            {
+    //                DO.Task? task = depend == null ? null : _dal.Task.Read(depend.ID);
+    //                if (task != null)
+    //                    _dal.Task.Update(task with { deadline = milestone.deadline });
+    //                return task;
+    //            }));                  
+    //}
 
-    public void CreateSchedual()
-    {
-        throw new NotImplementedException();
-    }
+    //public void CreateSchedual()
+    //{
+    //    throw new NotImplementedException();
+    //}
 }
